da93cdb40f1f074e477a05d28f09602f3a173393
NDThuong <100913702+NDThuong@users.noreply.github.com>
2022-07-22 10:34:18 +0700

    Update istio.sh

--- istio.sh
+++ istio.sh
@@ -1,4 +1,7 @@
 #istio
+    minikube delete
+    minikube start --driver=none --kubernetes-version v1.23.8
     helm repo add istio https://istio-release.storage.googleapis.com/charts
     helm repo update
     kubectl create namespace istio-system
+    kubectl create namespace istio-ingress
