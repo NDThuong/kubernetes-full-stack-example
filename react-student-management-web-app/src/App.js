997caf7fac70da4b7c03d853a1b60c7bb272dfe7
NDThuong <100913702+NDThuong@users.noreply.github.com>
2022-07-25 16:15:27 +0700

    Update App.js

--- react-student-management-web-app/src/App.js
+++ react-student-management-web-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/students" className="navbar-brand">
-            Nguyen Thi Dieu Thuong
+            NTDT
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
