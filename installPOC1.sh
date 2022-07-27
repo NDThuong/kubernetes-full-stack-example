4d6e5e707843aaf5acf55f7dad53ed8a639b2602
NDThuong <100913702+NDThuong@users.noreply.github.com>
2022-07-27 09:17:12 +0700

    Update installPOC1.sh

--- installPOC1.sh
+++ installPOC1.sh
@@ -6,7 +6,7 @@
     wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
     sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
     sudo apt-get install jenkins -y
-    sudo service jenkins status
+    sudo service jenkins start
 
 #docker
     sudo apt install apt-transport-https ca-certificates curl software-properties-common
