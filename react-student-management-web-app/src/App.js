55486a1171db69b550af00d110f0b391d9ff39f2
NDThuong <thuong.nguyen@hcl.com>
2022-07-29 02:50:00 +0000

    1

--- react-student-management-web-app/src/App.js
+++ react-student-management-web-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/students" className="navbar-brand">
-            Thuong Nguyen HCL
+            HCL DevOps Team Viet Nam
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
